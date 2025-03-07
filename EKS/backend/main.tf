3a5d1a3a921b31e09b253e98d6ad702efc5dbdbd
Pavana Paradesi <pp318@student.le.ac.uk>
2025-03-07 12:57:42 +0530

    changes to backed main

--- EKS/backend/main.tf
+++ EKS/backend/main.tf
@@ -1,11 +1,30 @@
 ## Creating S3 bucket for storing state file in the S3 bucket - Backend
 # "s3_bucket" is not the name of the S3 bucket, its the name of the resource "S3"
 
-resource "aws_s3_bucket" "s3_bucket" {
+resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = var.bucket_name
 
   lifecycle {
     prevent_destroy = false
+  }
+}
+
+# Versioning enabled for S3 bucket
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+# Server side encryption enabled for S3 bucket
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
   }
 }
 
@@ -17,9 +36,6 @@ resource "aws_dynamodb_table" "dynamodb-table" {
   name           = var.dynamodb_name
   billing_mode   = var.billing_mode
   hash_key       = var.hash_key
-  read_capacity  = 1
-  write_capacity = 1
-
   attribute {
     name = "LockId"
     type = "S"
